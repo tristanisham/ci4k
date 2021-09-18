1068f77173ba5b291242aaa1cb31f9d87c6d349b
Tristan Isham <tristan@isham.co>
2021-09-18 02:12:45 -0400

    This might work fully

--- ci4k.cpp
+++ ci4k.cpp
@@ -9,16 +9,16 @@
 
 using namespace std;
 
-void DesktopResolution(int& horizontal, int& vertical) {
-    RECT desktop;
-
-    const HWND hDesktop = GetDesktopWindow();
-
-    GetWindowRect(hDesktop, &desktop);
-
-    horizontal = desktop.right;
-    vertical = desktop.bottom;
-}
+//void DesktopResolution(int& horizontal, int& vertical) {
+//    RECT desktop;
+//
+//    const HWND hDesktop = GetDesktopWindow();
+//
+//    GetWindowRect(hDesktop, &desktop);
+//
+//    horizontal = desktop.right;
+//    vertical = desktop.bottom;
+//}
 
 void timer(function<void(void)> func, unsigned int interval) {
     thread([func, interval]() {
@@ -31,13 +31,23 @@ void timer(function<void(void)> func, unsigned int interval) {
     }).detach();
 }
 
+void take_screenshot() {
+    keybd_event(VK_LWIN, 0, 0, 0); // Left Windows Key down
+    keybd_event(VK_SNAPSHOT, 0, 0, 0); // Prnt Scrn down
+
+    keybd_event(VK_SNAPSHOT, 0, KEYEVENTF_KEYUP, 0); // Left Windows Key up
+    keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0); // Prnt Scrn up
+}
+
 int main()
 {
-        std::cout << "ci4k v0.0.1\n";
-        int horizontal = 0;
-        int vertical = 0;
+        std::cout << "ci4k v1.0.0\n";
+        /*int horizontal = 0;
+        int vertical = 0;*/
         
-        DesktopResolution(horizontal, vertical);
+        //DesktopResolution(horizontal, vertical);
+        timer(take_screenshot, 10000);
+        
         
         return 0;
 }
